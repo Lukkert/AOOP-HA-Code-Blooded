7cae941dcddfa55e298194fd8c7135cd69409e8d
lukkert <luekkert24@student.sdu.dk>
2025-03-12 15:34:30 +0100

    Initialize Homeassignment 2

---
+++ HA2/ScheduleApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ScheduleApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ HA2/ScheduleApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ScheduleApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
